7c1511f4005eb256a4b6ce3c873c7f34451dfcba
Jose Guillermo Lopez <joeeeeeyy@icloud.com>
2020-11-12 17:55:17 -0800

    Initialize repository.

---
+++ lib/jlopez_palindrome.rb
@@ -0,0 +1,6 @@
+require "jlopez_palindrome/version"
+
+module JlopezPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jlopez_palindrome/version.rb
@@ -0,0 +1,3 @@
+module JlopezPalindrome
+  VERSION = "0.1.0"
+end
